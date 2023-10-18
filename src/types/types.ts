1e5803e7f8c0c1c0a3a3f64b1ba24c09071d8877
ramiro elisei <ramiroelisei@gmail.com>
2023-10-18 23:06:28 +0200

    Add character types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,27 @@
-interface CharacterData {
+import type Character from "../Character/Character.js";
+import { type Fighter } from "../Fighter/Fighter.js";
+
+export interface CharacterData {
   name: string;
   family: string;
   age: number;
   imageSource: string;
 }
 
-export default CharacterData;
+export interface KingData extends CharacterData {
+  yearsOfReign: number;
+}
+
+export interface FighterData extends CharacterData {
+  weapon: string;
+  dexterity: number;
+}
+
+export interface SquireData extends CharacterData {
+  kissAssLevel: number;
+  serves: Fighter;
+}
+
+export interface AdvisorData extends CharacterData {
+  advises: Character;
+}
