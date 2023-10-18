6d96a5c666885a695c85d975b13161c1d33f7d0a
ramiro elisei <ramiroelisei@gmail.com>
2023-10-18 22:01:37 +0200

    Add App component

---
+++ src/components/App/App.ts
@@ -0,0 +1,9 @@
+import Component from "../Main-component/Main-component.js";
+
+class App extends Component {
+  populate(): void {
+    throw new Error("Method not implemented.");
+  }
+}
+
+export default App;
