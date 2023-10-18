dc7e491480870fa6c2d7d97ca0584e8d4829649e
ramiro elisei <ramiroelisei@gmail.com>
2023-10-18 22:38:25 +0200

    Add characters list in data

---
+++ src/data/characters.ts
@@ -0,0 +1,47 @@
+import { Advisor } from "../Advisor/Advisor";
+import { Fighter } from "../Fighter/Fighter";
+import King from "../King/King";
+import { Squire } from "../Squire/Squire";
+
+const joffreyCharacterData = {
+  name: "Joffrey",
+  family: "Baratheon",
+  age: 16,
+  imageSource: "joffrey.jpg",
+};
+
+const jaimeCharacterData = {
+  name: "Jaime",
+  family: "Lannister",
+  age: 45,
+  imageSource: "jaime.jpg",
+};
+
+const daenerysCharacterData = {
+  name: "Daenerys",
+  family: "Targaryen",
+  age: 30,
+  imageSource: "daenerys.jpg",
+};
+
+const tyrionCharacterData = {
+  name: "Tyrion",
+  family: "Lannister",
+  age: 40,
+  imageSource: "tyrion.jpg",
+};
+
+const bronnCharacterData = {
+  name: "Bronn",
+  family: "Blackwater",
+  age: 54,
+  imageSource: "bronn.jpg",
+};
+
+const joffrey = new King(joffreyCharacterData, 2);
+const jaime = new Fighter(jaimeCharacterData, "sword", 7);
+const daenerys = new Fighter(daenerysCharacterData, "Dragons", 10);
+const tyrion = new Advisor(tyrionCharacterData, daenerys);
+const bronn = new Squire(bronnCharacterData, 0, jaime);
+
+export const characters = [jaime, joffrey, daenerys, tyrion, bronn];
