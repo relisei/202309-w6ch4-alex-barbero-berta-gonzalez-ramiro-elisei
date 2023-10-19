b10364391bd2428a9653283779c8e2597cb4ff07
Alex Barbero <barrufat@hotmail.com>
2023-10-19 21:56:26 +0200

    Add character interfaces

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,3 +5,20 @@ export interface CharacterData {
   imageSource: string;
 }
 
+export interface King extends CharacterData {
+  yearsOfReing: number;
+}
+
+export interface Fighter extends CharacterData {
+  weapon: string;
+  dexterity: number;
+}
+
+export interface Advisor extends CharacterData {
+  advises: King | Advisor | Fighter | Squire;
+}
+
+export interface Squire extends CharacterData {
+  kissAssLevel: number;
+  serves: Fighter;
+}
