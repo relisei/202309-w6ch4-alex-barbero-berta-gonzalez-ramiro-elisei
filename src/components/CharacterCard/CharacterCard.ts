587ce504ee7cafb05b14abe4d1fa8f668853edee
Alex Barbero <barrufat@hotmail.com>
2023-10-19 20:22:02 +0200

    Add overlay and styles to card

--- src/components/CharacterCard/CharacterCard.ts
+++ src/components/CharacterCard/CharacterCard.ts
@@ -16,22 +16,43 @@ class CharacterCard extends Component {
     this.element.innerHTML = `
       <article class="character">
         <div class="card character__card">
-          <img src="img/${this.character.imageSource}" alt="${this.character.name} and ${this.character.family}" class="character__picture card-img-top" />
+          <img src="img/${this.character.imageSource}" alt="${
+            this.character.name
+          } and ${
+            this.character.family
+          }" class="character__picture card-img-top" />
           <div class="card-body">
-            <h2 class="character__name card-title h4">${this.character.name} & ${this.character.family}</h2>
+            <h2 class="character__name card-title h4">${
+              this.character.name
+            } & ${this.character.family}</h2>
             <div class="character__info">
               <ul class="list-unstyled">
                 <li>Age: ${this.character.age} yrs</li>
                 <li>
                   State: ${
-                    this.state 
-                    ? `<i class="fas fa-thumbs-down"></i>`
-                    : `<i class="fas fa-thumbs-up"></i>`
+                    this.state
+                      ? `<i class="fas fa-thumbs-down"></i>`
+                      : `<i class="fas fa-thumbs-up"></i>`
                   }
                 </li>
               </ul>
             </div>
+            <div class="character__overlay">
+                <ul class="list-unstyled">
+                  <li>Years of Reign: X</li>
+                  <li>Weapon: XXX</li>
+                  <li>Dexterity: X</li>
+                  <li>Kiss Ass level: X</li>
+                  <li>Advises to: X</li>
+                  <li>Serves to: X</li>
+                </ul>
+                <div class="character__actions">
+                  <button class="character__action btn">speak</button>
+                  <button class="character__action btn">die</button>
+                </div>
+              </div>
           </div>
+            <i class="emoji"></i>          
         </div>
       </article>
       `;
